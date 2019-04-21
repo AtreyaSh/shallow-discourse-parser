067e2c304e1fefafa20e9da67f76134b818b7179
Atreya Shankar <35427332+AtreyaSh@users.noreply.github.com>
2019-04-21 16:42:31 +0200

    *shankar* init.sh bugfix

--- init.sh
+++ init.sh
@@ -9,4 +9,4 @@ cp ./hooks/pre-commit-pipreqs.sample ./.git/hooks/pre-commit
 # https://gist.github.com/yanaiela/cfef50380de8a5bfc8c272bb0c91d6e1
 cd hooks && ./word2vec-download300model.sh ../tmp && cd ../tmp && \
 gunzip -k GoogleNews-vectors-negative300.bin.gz && \
-mv GoogleNews-vectors-negative300.bin ../implicit_nn_training/data/
+mv GoogleNews-vectors-negative300.bin ../src/data/
