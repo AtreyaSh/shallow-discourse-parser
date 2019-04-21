f10d82ef529c69ce5fc7b306764a6528e8641c41
Atreya Shankar <35427332+AtreyaSh@users.noreply.github.com>
2019-04-21 23:10:50 +0200

    *shankar* minor refinement

--- src/training/train_embedding.py
+++ src/training/train_embedding.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-####################################
-# import dependencies
-####################################
-
 import json
 import gensim
 import logging
